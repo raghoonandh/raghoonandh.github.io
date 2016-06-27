3262df631844b0d822a348672d18418a0bec182f
raghunandh <raghoo10@yahoo.com>
2016-06-27 21:26:02 +0530

    leftback added

--- football/emojifootball.js
+++ football/emojifootball.js
@@ -1,16 +1,26 @@
+
+var w = 600
+var h = 900
+formation = [3,4,2]
 var svg = d3.select('#emojifootball')
 			.append('svg')
-			.attr('width', 400)
-			.attr('height',600)
+			.attr('width', 600)
+			.attr('height',900)
 				 svg.append("svg:image")
   					.attr("xlink:href", 'img/football-field.svg')
-  					.attr("width", 400)
- 					.attr("height", 600)
+  					.attr("width", w)
+ 					.attr("height", h)
   					.attr("x", 10)
   					.attr("y", 10);
-				 svg.append("svg:image")
-  					.attr("xlink:href", 'img/happy.svg')
-  					.attr("width", 20)
- 					.attr("height", 20)
-  					.attr("x", 80)
-  					.attr("y", 150);
+
+function appendleftback(svg,formation,performance)
+{
+           svg.append("svg:image")
+            .attr("xlink:href", 'img/'+ performance +'.svg')
+            .attr("width", 20)
+            .attr("height", 20)
+            .attr("x", function(){return formation[0]==4? w/4.5:w/3.25 })
+            .attr("y", h/3);
+}
+
+appendleftback(svg, formation, 'good')
