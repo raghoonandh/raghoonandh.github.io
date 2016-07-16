71e765e65a7146cec2485e3f3a7be536153dfdfe
raghunandh <raghoo10@yahoo.com>
2016-07-16 17:15:53 +0530

    headers added

--- football/fpl/fpl.js
+++ football/fpl/fpl.js
@@ -1,9 +1,47 @@
 
 var fixturestable = d3.selectAll('#fixturestable')
   					  .append('svg')
-  					  .attr("viewBox", "0 0 300 200")
+  					  .attr("viewBox", "0 0 300 180")
 
-var height = 180
+                   fixturestable.append('rect')
+                              .attr('x', 70)
+                              .attr('y', 0)
+                              .attr('height',8)
+                              .attr('width',25)
+                              .attr('fill','red')
+                  fixturestable.append('text')
+                               .attr('x',70)
+                               .attr('y',4)
+                               .attr('dy','0.37em')
+                               .attr('class', 'header')
+                               .text('Next GW')
+                   fixturestable.append('rect')
+                              .attr('x', 100)
+                              .attr('y', 0)
+                              .attr('height',8)
+                              .attr('width',25)
+                              .attr('fill','blue')
+                  fixturestable.append('text')
+                               .attr('x',100)
+                               .attr('y',4)
+                               .attr('dy','0.37em')
+                               .attr('class', 'header')
+                               .text('Next 3 GWs')
+                   fixturestable.append('rect')
+                              .attr('x', 130)
+                              .attr('y', 0)
+                              .attr('height',8)
+                              .attr('width',25)
+                              .attr('fill','orange')
+                  fixturestable.append('text')
+                               .attr('x',130)
+                               .attr('y',4)
+                               .attr('dy','0.37em')
+                               .attr('class', 'header')
+                               .text('Next 6 GWs')
+
+
+var height = 160
 d3.csv("mock.csv", function(error, data) {
     // data.forEach(function(d) {
     //     d['1gw'] = +d['1gw'];  
@@ -17,7 +55,7 @@ d3.csv("mock.csv", function(error, data) {
 
     var yScale = d3.scale.linear()
                    		 .domain([0, d3.max(data, function (d) { return +d['1gw']})])
-                   		 .rangeRound([10, height]);
+                   		 .rangeRound([20, height]);
     var x = 20
     var x1 = 80
 
@@ -39,7 +77,7 @@ d3.csv("mock.csv", function(error, data) {
                                 .attr("y", function(d){ return yScale(parseInt(d['1gw'])) })
 
                              var rectsgw3 = groups.append("rect")
-                                .attr('x',x1+20)
+                                .attr('x',x1+30)
                                 .attr('y', 10 )
                                 .attr('height', 5)
                                 .attr('width', 5)
@@ -51,7 +89,7 @@ d3.csv("mock.csv", function(error, data) {
                                 .attr("y", function(d){ return yScale(parseInt(d['3gw'])) })
                              
                              var rectsgw6 = groups.append("rect")
-                                .attr('x',x1+40)
+                                .attr('x',x1+60)
                                 .attr('y', 10 )
                                 .attr('height', 5)
                                 .attr('width', 5)
