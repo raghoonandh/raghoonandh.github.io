4d3753b8458472cf0be5c8df1b2843b4ea168ff7
raghunandh <raghoo10@yahoo.com>
2016-12-22 16:12:15 +0530

    minor UI fixes

--- TNEA-2016/heatmap.js
+++ TNEA-2016/heatmap.js
@@ -171,6 +171,6 @@ var docHeight = $(window).height();
 var footerHeight = $('.footer').height();
 var footerTop = $('.footer').position().top + footerHeight;  
 if (footerTop < docHeight) {
-$('.footer').css('margin-top',  (docHeight - footerTop) + 'px');
+$('.footer').css('margin-top', 5 + (docHeight - footerTop) + 'px');
 }
 });
