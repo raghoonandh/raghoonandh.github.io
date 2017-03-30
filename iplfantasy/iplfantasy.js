cf3868dda47a56166e2c0abf5fc6dc761e55c963
raghunandh <raghoo10@yahoo.com>
2017-03-30 16:13:21 +0530

    metatags and google analytics added

--- iplfantasy/iplfantasy.js
+++ iplfantasy/iplfantasy.js
@@ -256,5 +256,18 @@ function toTitleCase(str)
       return ''
 }
 
+setTimeout(function() { ohsnapalert() }, 25000);
+
+function ohsnapalert(){
+  ohSnap('This App Auto Updates After Everymatch!', {color: 'green', 'duration':'8000'});
+}
 
 
+// google-analytics
+ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-78417578-1', 'auto');
+  ga('send', 'pageview');
